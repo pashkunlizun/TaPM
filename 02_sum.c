782ddf6ccacd1f50f74765fcd60f1151526420d3
des <37875869+greedyfox7@users.noreply.github.com>
2019-02-16 15:36:32 +0700

    Add files via upload

--- 02_sum.c
+++ 02_sum.c
@@ -1,10 +1,8 @@
-#include<stdio.h>
-#include<math.h>
-int main()
-{
-    int a ,b;
-    printf("Please enter the number and its degree: ");
+#include <stdio.h>
+
+int main() {
+    int a, b;
     scanf("%d %d", &a, &b);
-    b=pow(a,b);
-    printf("%d",b);
+    printf("\n%d", a+b);
+    return 0;
 }
