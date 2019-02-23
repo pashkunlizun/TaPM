9bc39490cd90cba4504acb2bdbbd076015016a91
des <37875869+greedyfox7@users.noreply.github.com>
2019-02-23 17:11:46 +0700

    Add files via upload

---
+++ 13_sumAllsqua.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int N ,n;
+    int i=0;
+    scanf("%d", &N);
+    int sum = 0;
+    for (i = 0; i < N; i++)
+    {
+    	scanf("%d", &n);
+    	sum += n*n;
+	}
+    printf("sum: %d",sum);
+}
