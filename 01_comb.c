0cb15c9e970eebefcf6b5d633cbdc4439a7414e8
des <37875869+greedyfox7@users.noreply.github.com>
2019-04-01 16:04:55 +0700

    Update 01_comb.c

--- 01_comb.c
+++ 01_comb.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 void comb(int *arr, int arr_length) {
-	int i,t,count=0, compare =0;
+	int i,t;
 	int step = arr_length;
 	double fakt = 1.2473309;
 	while (step >= 1)
@@ -11,19 +11,13 @@ void comb(int *arr, int arr_length) {
 		{
 			if (arr[i] > arr[i+step])
 			{
-				compare++;
-				count++;
 				t=arr[i];
 				arr[i] = arr[i+step];
 				arr[i+step] = t;
 			}
-			else {
-                compare++;
-			}
 		}
 		step /=fakt;
 	}
-	    printf("number of permutations(perestanovki) = %d \nnumber of comparisons(sravnenie) = %d \n\n",count, compare);
 }
 
 int print(int *arr, int arr_len)	{
